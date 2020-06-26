5950f11e1730275b72ecfe29663954b6e36d6866
Yati <yatimisi2018@gmail.com>
2020-06-26 16:58:19 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,7 @@ function main() {
          && { git clone https://github.com/zsh-users/antigen.git $HOME/.antigen;} \
          || ( mkdir "$HOME/.antigen" ; curl -#L https://github.com/zsh-users/antigen/tarball/develop | tar -xz --strip-components 1 -C $HOME/.antigen;)
 
+        echo "Done. $HOME/.antigen"
         echo ""
     fi;
 
@@ -23,12 +24,13 @@ function main() {
          && { git clone https://github.com/tmux-plugins/tpm.git $HOME/.tmux/plugins/tpm;} \
          || ( mkdir "$HOME/.tmux/plugins/tpm" ; curl -#L https://github.com/tmux-plugins/tpm/tarball/master | tar -xz --strip-components 1 -C $HOME/.tmux/plugins/tpm;)
 
+        echo "Done. $HOME/.tmux/plugins/tpm"
         echo ""
     fi;
 
     # Make directory ----------------------------------------------------------------
     for dir in ~/.{pip,environment}; do
-        [ ! -d "$dir" ] && mkdir "$dir"
+        [ ! -d "$dir" ] && mkdir "$dir" && echo "Done. $dir"
     done
 
     # Made environment --------------------------------------------------------------
@@ -41,18 +43,39 @@ function main() {
     for ((i=0; i < ${#ENV[@]}; i++))
     do
         if [ ! -f "$ENV_PATH/.$(basename "${ENV[$i]}")" ]; then
-            cp ${ENV[$i]} "$ENV_PATH/.$(basename "${ENV[$i]}")"
+            cp ${ENV[$i]} "$ENV_PATH/.$(basename "${ENV[$i]}")" && echo "Done. $ENV_PATH/.$(basename "${ENV[$i]}")"
         fi
     done
 
     # Link config -------------------------------------------------------------------
-    [ ! -d "$HOME/.dotfiles" ] && ln -si "$(dirname "${BASH_SOURCE}")" $HOME/.dotfiles
-    ln -si $HOME/.dotfiles/zsh/zshrc.zsh $HOME/.zshrc
-    ln -si $HOME/.dotfiles/config/gitconfig/gitconfig $HOME/.gitconfig
-    ln -si $HOME/.environment/.pypirc $HOME/.pypirc
-    ln -si $HOME/.dotfiles/config/pip.conf $HOME/.pip/pip.conf
-    ln -si $HOME/.dotfiles/config/tmux.conf $HOME/.tmux.conf
-    ln -si $HOME/.dotfiles/config/cz-conventional/czrc $HOME/.czrc
+    [ ! -d "$HOME/.dotfiles" ] && ln -si "$(dirname "${BASH_SOURCE}")" $HOME/.dotfiles && echo "Done. $HOME/.dotfiles"
+
+    FORM_LINK=(
+        "$HOME/.dotfiles/zsh/zshrc.zsh"
+        "$HOME/.dotfiles/config/gitconfig/gitconfig"
+        "$HOME/.environment/.pypirc"
+        "$HOME/.dotfiles/config/pip.conf"
+        "$HOME/.dotfiles/config/tmux.conf"
+        "$HOME/.dotfiles/config/cz-conventional/czrc"
+    )
+
+    TO_LINK=(
+        "$HOME/.zshrc"
+        "$HOME/.gitconfig"
+        "$HOME/.pypirc"
+        "$HOME/.pip/pip.conf"
+        "$HOME/.tmux.conf"
+        "$HOME/.czrc"
+    )
+
+    for ((i=0; i < ${#FORM_LINK[@]}; i++))
+    do
+        if [ "$1" == "--force" -o "$1" == "-f" ]; then
+            [ ! -f ${TO_LINK[$i]} ] && ln -sh ${FORM_LINK[$i]} ${TO_LINK[$i]} && echo "Done. ${TO_LINK[$i]}"
+        else
+            ln -si ${FORM_LINK[$i]} ${TO_LINK[$i]} && echo "Done. ${TO_LINK[$i]}"
+        fi;
+    done
 
     if [ $(uname) != "Darwin" ]; then  # No conky for OSX
         if [ -d "$HOME/.local/share/applications" ]; then  # No conky for no GUI
@@ -73,8 +96,8 @@ function main() {
         "pipenv"
     )
 
-    echo ""
-    echo "Down."
+    echo "-------------------------------------------------------------------"
+    echo "Done."
     echo ""
     echo "Please modify '.environment/' in your home dir."
     echo ""
@@ -92,7 +115,7 @@ function main() {
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	main;
+	main -f;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
